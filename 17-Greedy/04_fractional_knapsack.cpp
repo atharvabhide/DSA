4fdf094ee8ef0fdd16ebd7f14e02df7575e03e33
Atharva Bhide <67187699+atharvabhide@users.noreply.github.com>
2022-06-14 23:24:15 +0530

    Create 04_fractional_knapsack.cpp

---
+++ 17-Greedy/04_fractional_knapsack.cpp
@@ -0,0 +1,40 @@
+/*
+struct Item{
+    int value;
+    int weight;
+};
+*/
+
+
+class Solution
+{
+    public:
+    //Function to get the maximum total value in the knapsack.
+    
+    static bool cmp(Item i1, Item i2)
+    {
+        double r1 = double(i1.value) / double(i1.weight);
+        double r2 = double(i2.value) / double(i2.weight);
+        return r1 > r2;
+    }
+    
+    double fractionalKnapsack(int W, Item arr[], int n)
+    {
+        sort(arr, arr+n, cmp);
+        double result = 0.0;
+        for (int i = 0; i < n; i++)
+        {
+            if (arr[i].weight <= W)
+            {
+                result += arr[i].value;
+                W -= arr[i].weight;
+            }
+            else
+            {
+                result += W*double(arr[i].value)/double(arr[i].weight);
+                return result;
+            }
+        }
+        return result;
+    }
+};
